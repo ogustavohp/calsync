8da759c7fb1fe0eebefe7557a1ce3445d34f40b6
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-12-23 03:06:21 -0300

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,29 @@
 import Image from 'next/image'
 import banner from '../assets/banner.png'
+import ClaimUsernameForm from '@/components/ClaimUsernameForm'
 
 export default function Home() {
   return (
     <div className="ml-auto flex h-screen max-w-[calc(100vw-(100vw-1160px)/2)] items-center gap-20 overflow-hidden">
       <div className="max-w-lg px-10">
-        <h1 className="text-4xl">Agendamento descomplicado</h1>
-        <p className="mt-2 text-xl text-gray-200">
+        <h1 className="text-6xl font-bold leading-tight">
+          Agendamento descomplicado
+        </h1>
+        <p className="mt-2 text-xl leading-tight text-zinc-400">
           Conecte seu calendário e permita que as pessoas marquem agendamentos
           no seu tempo livre.
         </p>
+        <ClaimUsernameForm />
       </div>
-      <div className="hidden min-w-[600px] overflow-hidden pr-8 sm:block">
-        <Image
-          src={banner}
-          alt="Calendário"
-          quality={100}
-          priority
-          height={400}
-        />
-      </div>
+
+      <Image
+        src={banner}
+        alt="Calendário"
+        quality={100}
+        priority
+        height={400}
+        className="hidden pr-8 sm:block"
+      />
     </div>
   )
 }
