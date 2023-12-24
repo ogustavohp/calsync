a17f9ef721ea97736971ee00f51a1b278959d877
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-12-24 18:40:17 -0300

    create claimUserNameForm.tsx

--- src/components/ClaimUserNameForm.tsx
+++ src/components/ClaimUserNameForm.tsx
@@ -1,56 +1,69 @@
 'use client'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { ArrowRight } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
 const claimUsernameFormSchema = z.object({
-  username: z.string(),
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisar ter três ou mais letras.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens.',
+    })
+    .transform((username) => username.toLowerCase()),
 })
 
 type claimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export default function ClaimUsernameForm() {
-  const { register, handleSubmit } = useForm<claimUsernameFormData>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<claimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  const router = useRouter()
 
   async function handleClaimUsername(data: claimUsernameFormData) {
-    console.log(data)
+    const { username } = data
+
+    await router.push(`/register?username=${username}`)
   }
 
-  // const prefix = 'calsync.com/'
-  // const [username, setUsername] = useState(prefix)
-  // const [typed, setTyped] = useState('')
-
-  // function handleUsername(e: ChangeEvent<HTMLInputElement>) {
-  //   if (e.target.value.length < 12) {
-  //     setUsername(`${prefix}${e.target.value}`)
-  //     setTyped(e.target.value)
-  //   } else {
-  //     const typedText = e.target.value.substring(12)
-  //     setUsername(`${prefix}${typedText}`)
-  //     setTyped(typedText)
-  //   }
-  // }
-
   return (
-    <form
-      onSubmit={handleSubmit(handleClaimUsername)}
-      className="mt-4 flex h-auto flex-col gap-2 rounded-md border border-zinc-700 bg-zinc-800 p-4 sm:flex-row"
-    >
-      <input
-        type="text"
-        id=""
-        placeholder="calsync.com/seu-usuário"
-        {...register('username')}
-        className="h-10 rounded-md bg-zinc-900 pl-2 focus:outline focus:ring-4 focus:ring-emerald-700 sm:flex-1"
-      />
-      <button
-        type="submit"
-        className="flex h-10 min-w-40 items-center justify-center rounded-md bg-emerald-700 font-bold"
+    <>
+      <form
+        onSubmit={handleSubmit(handleClaimUsername)}
+        className="mb-1 mt-4 flex h-auto flex-col gap-2 rounded-md border border-zinc-700 bg-zinc-800 p-4 sm:flex-row"
       >
-        Reservar usuário
-        <ArrowRight />
-      </button>
-    </form>
+        <input
+          type="text"
+          id=""
+          placeholder="calsync.com/seu-usuário"
+          {...register('username')}
+          className="h-10 rounded-md bg-zinc-900 pl-2 focus:outline focus:ring-4 focus:ring-emerald-700 sm:flex-1"
+        />
+        <button
+          type="submit"
+          disabled={isSubmitting}
+          className="flex h-10 min-w-40 items-center justify-center rounded-md bg-emerald-700 font-bold"
+        >
+          Reservar usuário
+          <ArrowRight />
+        </button>
+      </form>
+      <span
+        className={`pl-4 text-sm ${
+          errors.username?.message ? 'text-red-400' : 'text-zinc-400'
+        }`}
+      >
+        {errors.username?.message || 'Digite o nome do usuário.'}
+      </span>
+    </>
   )
 }
