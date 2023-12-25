af8f8f4af81da40ec10a868f1f429ea7b1e06445
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-12-25 08:38:39 -0300

    change nextauth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,50 +1,5 @@
-import { PrismaAdapter } from '@/lib/auth/prisma-adapter'
+import { authOptions } from '@/lib/auth/auth'
 import NextAuth from 'next-auth'
-import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
 
-const handler = NextAuth({
-  adapter: PrismaAdapter(),
-
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-      authorization: {
-        params: {
-          scope:
-            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
-        },
-      },
-      profile(profile: GoogleProfile) {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          username: '',
-          email: profile.email,
-          avatar_url: profile.picture,
-        }
-      },
-    }),
-  ],
-
-  callbacks: {
-    async signIn({ account }) {
-      if (
-        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
-      ) {
-        return '/register/connect-calendar/?error=permissions'
-      }
-
-      return true
-    },
-
-    async session({ session, user }) {
-      return {
-        ...session,
-        user,
-      }
-    },
-  },
-})
-
+const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
