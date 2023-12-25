16b05ddc8d683c2f71233a85885d13666b4f687d
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-12-24 21:18:30 -0300

    create POST api/users route

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,40 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '@/lib/prisma'
+import { NextRequest, NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(req.body)
+export async function POST(request: NextRequest) {
+  const { name, username } = await request.json()
+  const cookieStore = cookies()
+
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (userExists) {
+    return NextResponse.json(
+      { error: 'Username already taken.' },
+      { status: 400 },
+    )
+  }
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      username,
+    },
+  })
+
+  cookieStore.set({
+    name: '@calsync/userId',
+    value: user.id,
+    httpOnly: true,
+    path: '/',
+    maxAge: 60 * 60 * 24 * 7, // 7days
+  })
+
+  // request.cookies.set({ name: '@calsync/userId', value: 'aaaa' })
+
+  return NextResponse.json(user, { status: 201 })
 }
