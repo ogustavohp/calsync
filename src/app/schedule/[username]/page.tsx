9b1c4b744127ec370943ac6ac82557cb8e32e017
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-12-29 18:31:34 -0300

    create schedule/[username] page

---
+++ src/app/schedule/[username]/page.tsx
@@ -0,0 +1,52 @@
+import { ScheduleForm } from '@/components/schedule/ScheduleForm'
+import { prisma } from '@/lib/prisma'
+import { User } from 'lucide-react'
+import Image from 'next/image'
+import { redirect } from 'next/navigation'
+
+interface ScheduleProps {
+  username: string
+}
+
+export default async function Schedule({ params }: { params: ScheduleProps }) {
+  const username = params.username
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return redirect('/404')
+  }
+
+  return (
+    <div className="mx-auto mb-4 mt-20 max-w-[852px] px-4">
+      {/* User */}
+      <div className="flex flex-col items-center">
+        {!user.avatar_url ? (
+          <User
+            width={100}
+            height={100}
+            className="rounded-full bg-zinc-500 p-2"
+          />
+        ) : (
+          <Image
+            width={100}
+            height={100}
+            quality={100}
+            src={user.avatar_url}
+            alt={user.name}
+            referrerPolicy="no-referrer"
+            className="rounded-full"
+          />
+        )}
+        <h2 className="leading">{user.name}</h2>
+        <p className="">{user.bio}</p>
+      </div>
+      {/* Calendar */}
+      <ScheduleForm />
+    </div>
+  )
+}
