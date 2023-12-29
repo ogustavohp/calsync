5fa303ed0bd7759a41aeb98c3f6b7cc7dcd4da64
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-12-29 18:31:59 -0300

    create calendar step component

---
+++ src/components/schedule/CalendarStep.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Calendar from '@/components/Calendar'
+
+export function CalendarStep() {
+  return (
+    <div className="relative col-span-1 mx-auto mb-0 mt-6 grid w-[540px] max-w-full">
+      <Calendar />
+    </div>
+  )
+}
