d2893f0ee1ca022d84f52321a42575b884d39918
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-12-29 22:14:24 -0300

    change scheduleForm component

--- src/components/schedule/ScheduleForm.tsx
+++ src/components/schedule/ScheduleForm.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { CalendarStep } from './CalendarStep'
+// import { CalendarStep } from './CalendarStep'
+import { ConfirmStep } from './ConfirmStep'
 
 export function ScheduleForm() {
-  return <CalendarStep />
+  return <ConfirmStep />
 }
