e26c99a42828b7d1959bd6cfd41c3eb9fe911aca
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-12-31 04:41:03 -0300

    create users/[username]/availability API route

---
+++ src/app/api/users/[username]/availability/route.ts
@@ -0,0 +1,81 @@
+import { prisma } from '@/lib/prisma'
+import dayjs from 'dayjs'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  // console.log(request.nextUrl)
+  const fullPathname = request.nextUrl.pathname.toLowerCase()
+  const fullPatchNameParts = fullPathname.split('/')
+
+  const usersIndex = fullPatchNameParts.indexOf('users')
+  const availabilityIndex = fullPatchNameParts.indexOf('availability')
+
+  const username = fullPatchNameParts
+    .slice(usersIndex + 1, availabilityIndex)
+    .toString()
+
+  const date = request.nextUrl.searchParams.get('date')
+
+  if (!date) {
+    return NextResponse.json({ error: 'Date not provided.' }, { status: 400 })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return NextResponse.json(
+      { error: 'Users does not exists.' },
+      { status: 400 },
+    )
+  }
+
+  const referenceDate = dayjs(String(date))
+  const isPastDate = referenceDate.endOf('day').isBefore(new Date())
+
+  if (isPastDate) {
+    return NextResponse.json({ possibleTimes: [], availableTimes: [] })
+  }
+
+  const userAvailability = await prisma.userTimeInterval.findFirst({
+    where: {
+      user_id: user.id,
+      week_day: referenceDate.get('day'),
+    },
+  })
+
+  if (!userAvailability) {
+    return NextResponse.json({ possibleTimes: [], availableTimes: [] })
+  }
+
+  const { time_start_in_minutes, time_end_in_minutes } = userAvailability
+  const startHour = time_start_in_minutes / 60
+  const endHour = time_end_in_minutes / 60
+
+  const possibleTimes = Array.from({ length: endHour - startHour }).map(
+    (_, i) => {
+      return startHour + i
+    },
+  )
+
+  const blockedTimes = await prisma.scheduling.findMany({
+    where: {
+      user_id: user.id,
+      date: {
+        gte: referenceDate.set('hour', startHour).toDate(),
+        lte: referenceDate.set('hour', endHour).toDate(),
+      },
+    },
+  })
+
+  const availableTimes = possibleTimes.filter((time) => {
+    return !blockedTimes.some(
+      (blockedTime) => blockedTime.date.getHours() === time,
+    )
+  })
+
+  return NextResponse.json({ possibleTimes, availableTimes })
+}
