89338c01f7c8ff580ddd1e90b26f73fe2e559811
Gustavo Henrique <gustavohpereira1.6@gmail.com>
2024-05-23 15:21:29 -0300

    prod commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
-import banner from '../assets/banner.png'
-import ClaimUsernameForm from '@/components/ClaimUsernameForm'
+import ClaimUsernameForm from '@/components/ClaimUserNameForm'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -11,12 +10,12 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="ml-auto flex h-screen max-w-[calc(100vw-(100vw-1160px)/2)] items-center gap-20 overflow-hidden">
+    <div className="mx-auto flex h-screen max-w-[calc(100vw-(100vw-1160px)/2)] items-center gap-20 overflow-hidden">
       <div className="max-w-lg px-10">
-        <h1 className="text-6xl font-bold leading-tight">
+        <h1 className="text-4xl font-bold leading-tight sm:text-6xl">
           Agendamento descomplicado
         </h1>
-        <p className="mt-2 text-xl leading-tight text-zinc-400">
+        <p className="mt-2 text-lg leading-tight text-zinc-400 sm:text-xl">
           Conecte seu calendário e permita que as pessoas marquem agendamentos
           no seu tempo livre.
         </p>
@@ -24,12 +23,13 @@ export default function Home() {
       </div>
 
       <Image
-        src={banner}
+        src={'/banner.png'}
         alt="Calendário"
         quality={100}
         priority
         height={400}
-        className="hidden pr-8 sm:block"
+        width={750}
+        className="hidden w-full pr-8 sm:block"
       />
     </div>
   )
